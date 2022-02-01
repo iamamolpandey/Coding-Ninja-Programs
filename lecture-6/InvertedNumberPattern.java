9004df850d2661a0415d33b12ae6b4c970e9f024
AMOL KUMAR PANDEY <87844443+iamamolpandey@users.noreply.github.com>
2022-02-01 21:49:49 +0530

    Inverted Number Pattern

---
+++ lecture-6/InvertedNumberPattern.java
@@ -0,0 +1,59 @@
+/*
+Code : Inverted Number Pattern
+
+Print the following pattern for the given N number of rows.
+Pattern for N = 4
+4444
+333
+22
+1
+Input format :
+Integer N (Total no. of rows)
+Output format :
+Pattern in N lines
+Constraints :
+0 <= N <= 50
+Sample Input 1:
+5
+Sample Output 1:
+55555 
+4444
+333
+22
+1
+Sample Input 2:
+6
+Sample Output 2:
+666666
+55555 
+4444
+333
+22
+1
+*/
+
+import java.util.Scanner;
+public class InvertedNumberPattern {
+
+
+	public static void main(String[] args) {
+		
+		/* Your class should be named Solution.
+	 	* Read input as specified in the question.
+	 	* Print output as specified in the question.
+		*/
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int i = n;
+        while(i>=1) {
+            int j = n;
+            while(j>=n-i+1){
+                System.out.print(i);
+				j--;
+            }
+            System.out.println();
+			i--;
+        }
+	}
+
+}
