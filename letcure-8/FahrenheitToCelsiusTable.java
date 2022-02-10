4ab1d6e22c891adf112ce1ba5c9923f7c58cd7cb
AMOL KUMAR PANDEY <87844443+iamamolpandey@users.noreply.github.com>
2022-02-10 23:44:30 +0530

    Update FahrenheitToCelsiusTable.java

--- letcure-8/FahrenheitToCelsiusTable.java
+++ letcure-8/FahrenheitToCelsiusTable.java
@@ -43,12 +43,7 @@ But we'll only print 48 because we are only interested in the integral part of t
 public class FahrenheitToCelsiusTable {
 
 	public static void printFahrenheitTable(int start, int end, int step) {
-		/* Your class should be named Solution 
-		 * Don't write main(). 
-		 * Don't read input, it is passed as function argument. 
-		 * Print the specified output in required format. 
-		 * Taking input is handled automatically. 
-		 */
+		
         for (int i = start; i <= end; i=i+step){
             int cel = (5*(i-32))/9;
             System.out.println(i + "\t" + cel);
