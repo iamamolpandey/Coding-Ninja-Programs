fede54848aa4b3bac3578a288e93d45c44e97a45
AMOL KUMAR PANDEY <87844443+iamamolpandey@users.noreply.github.com>
2022-02-12 23:15:44 +0530

    Update LinearSearch.java

--- lecture-9/LinearSearch.java
+++ lecture-9/LinearSearch.java
@@ -44,3 +44,15 @@ Sample Output 2:
 -1
 2
 */
+
+public class LinearSearch {
+
+    public static int linearSearch(int arr[], int x) {
+		for(int  i = 0; i<arr.length;i++){
+            if(arr[i]==x){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
